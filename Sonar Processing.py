6f3a8762885d22358d258795ee2726338f89c993
Steve <38364294+stevetasticsteve@users.noreply.github.com>
2018-05-23 12:18:39 +0200

    Add files via upload

--- Sonar Processing.py
+++ Sonar Processing.py
@@ -1,9 +1,14 @@
-import serial, pygame, math, time, traceback
+##from serial import serial
+import pygame, math, time, traceback, serial
 import logging
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+logging.basicConfig(level=logging.WARNING, format=' %(asctime)s - %(levelname)s- %(message)s')
 
 ser = serial.Serial('COM6',9600)
 pygame.init()
+#Define font
+pygame.font.init()
+fontsize = 18
+font=pygame.font.SysFont('Comic Sans MS',fontsize)
 
 # Define some colors
 BLACK    = (   0,   0,   0)
@@ -14,8 +19,9 @@ BLUE     = (   0,   0, 255)
 #Colours
 axiscolour=GREEN
 pingcolour=RED
+textcolour=WHITE
 #Define Size
-size = (700, 400)
+size = (1000, 600)
 xsize = size[0]
 xmid=xsize/2
 ysize = size[1]
@@ -49,16 +55,31 @@ while not done:
         screen.fill(BLACK)
         pygame.draw.line(screen, axiscolour, [0,ysize-Footerheight],[xsize,ysize-Footerheight])
         pygame.draw.circle(screen, axiscolour,(int(xmid),int(ysize-Footerheight)),int(xmid),1) #Large circle
-        pygame.draw.circle(screen, axiscolour,(int(xmid),int(ysize-Footerheight)),int(xmid*0.667),1)  #2/3 Circle
-        pygame.draw.circle(screen, axiscolour,(int(xmid),int(ysize-Footerheight)),int(xmid*0.33),1)  #1/3 Circle
+        pygame.draw.circle(screen, axiscolour,(int(xmid),int(ysize-Footerheight)),int(xmid*0.75),1)  #3/4 Circle
+        pygame.draw.circle(screen, axiscolour,(int(xmid),int(ysize-Footerheight)),int(xmid*0.5),1)  #1/2 Circle
+        pygame.draw.circle(screen, axiscolour,(int(xmid),int(ysize-Footerheight)),int(xmid*0.25),1)  #1/4 Circle
         pygame.draw.rect(screen, BLACK, [0,ysize,xsize,-(Footerheight-2)],0)
+        m1 = font.render('1m', False, (WHITE))
+        m2 = font.render('2m', False, (WHITE))
+        m3 = font.render('3m', False, (WHITE))
+        m4 = font.render('4m', False, (WHITE))
+        textoffset = 2
+        screen.blit(m1,(xmid-xmid*0.25,ysize-Footerheight+textoffset))
+        screen.blit(m2,(xmid-xmid*0.5,ysize-Footerheight+textoffset))
+        screen.blit(m3,(xmid-xmid*0.75,ysize-Footerheight+textoffset))
+        screen.blit(m4,(xmid-xmid,ysize-Footerheight+textoffset))
         pygame.display.flip()
         linesdrawn=0
 
-        while linesdrawn <179:
-            
+        while linesdrawn <180:
+            if event.type == pygame.QUIT:
+                pygame.quit
+                break
             if ser.in_waiting > 7:
                 data=ser.readline()
+                if len(data) <6:
+                    logging.warning('bad data recieved: '+ str(data))
+                    break
                 logging.debug('into read loop')
                 #logging.info(data)
                 deg =[]
@@ -85,6 +106,11 @@ while not done:
                     dist=int((str(dist[0]))+str(dist[1]))
                 elif len(dist) ==3:
                     dist=int((str(dist[0]))+str(dist[1])+str(dist[2]))
+                elif len (dist) >3:
+                    logging.warning('suspect distance reading' + str(dist))
+                    dist = 400
+                if dist > 400:
+                    dist = 400
                 logging.info('dist =' +str(dist))
                 scaleRatio = 400/xmid    #400cm is max range of sensor
                 scaleDist = scaleRatio * dist
